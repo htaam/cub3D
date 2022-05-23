45ca29de0e85e7859f605d7e7c2d0a0b9d03676e
marymota <dg.marciamota@gmail.com>
2022-05-24 00:16:03 +0100

    correct seg fault when ext. has more than 3 chars

--- src/check_args.c
+++ src/check_args.c
@@ -11,13 +11,22 @@ int	check_args(int argc, char *argv[])
 int check_extension (char *ext)
 {
 	int i;
+	char *str;
 
 	i = -1;
 	while (ext[++i])
 	{
 		if (ext[i] == '.' && i++)
-			if (ft_strncmp(&ext[i],"cub", 3) != 0)
+		{
+			str = &ext[i];
+			if (ft_strlen(str) == 3)
+			{
+				if (ft_strncmp(&ext[i],"cub", 3) != 0)
+					error_exit("File has wrong extension!");
+			}
+			else
 				error_exit("File has wrong extension!");
+		}
 	}
 	return (EXIT_SUCCESS);
 }
