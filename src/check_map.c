95eca477c9543964250f4853acded46bb3c6a595
marymota <dg.marciamota@gmail.com>
2022-05-25 13:41:35 +0100

    board is closed checks implementd

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: marmota <marmota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 17:52:49 by marmota           #+#    #+#             */
-/*   Updated: 2022/05/24 22:54:05 by marmota          ###   ########.fr       */
+/*   Updated: 2022/05/25 13:20:22 by marmota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,9 @@ int	empty_line(t_game *game, int i)
 
 void	map_isclosed(t_game *game, int i)
 {
-	int		hst;
-	int		wst;
 	size_t	j;
 
-	hst = 0;
-	wst = 0;
+
 	while (empty_line(game, i))
 		++i;
 	while (i < game->board_height)
@@ -44,27 +41,21 @@ void	map_isclosed(t_game *game, int i)
 		j = 0;
 		while (j < ft_strlen(game->board[i]))
 		{
-			if (!hst || i == game->board_height)
-				if (ft_isalnum(game->board[i][j]) && game->board[i][j] != '1')
-					error_exit("Board is not closed1");
-
-					
-			if (!wst || j == ft_strlen(game->board[i]))
+			if (i < game->board_height - 1)
+			{
+				if (!ft_isalnum(game->board[i - 1][j]) || !ft_isalnum(game->board[i][j + 1]) || !ft_isalnum(game->board[i][j - 1]) || !ft_isalnum(game->board[i + 1][j]))
+					if (ft_isalnum(game->board[i][j]) && game->board[i][j] != '1')
+						error_exit("Board is not closed1");
+			}
+			else if (i == game->board_height - 1)
+			{
 				if (ft_isalnum(game->board[i][j]) && game->board[i][j] != '1')
 					error_exit("Board is not closed2");
-			if (!ft_isalnum(game->board[i - 1][j]) || !ft_isalnum(game->board[i][j + 1]) || !ft_isalnum(game->board[i][j - 1]))
-				if (ft_isalnum(game->board[i][j]) && game->board[i][j] != '1')
-					error_exit("Board is not closed3");
-			if (i == game->board_height)
-				if (ft_isalnum(game->board[i][j]) && game->board[i][j] != '1')
-					if (game->board[i][j] != '1')
-						error_exit("Board is not closed4");
+			}
 			printf("%c", game->board[i][j]);
-			wst = 1;
 			++j;
 		}
 		printf("\n");
-		hst = 1;
 		++i;
 		//while (empty_line(game, i) && ++i <= game->board_height - 1)
 		//{
