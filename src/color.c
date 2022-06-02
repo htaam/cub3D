77eec1677d5a546eb88c615c37c99fa8225b1901
marymota <dg.marciamota@gmail.com>
2022-06-02 19:02:31 +0100

    solved bug with printf

--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: marmota <marmota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 17:12:47 by marmota           #+#    #+#             */
-/*   Updated: 2022/06/02 13:56:51 by marmota          ###   ########.fr       */
+/*   Updated: 2022/06/02 19:01:59 by marmota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ void	init_rgb(t_game *game, int n, char *s, int c)
 	}
 	if (n < 0 || n > 255)
 		error_exit("Invalid RGB value");
-	printf("rgb: %i,\n", n);
 }
 
 void	check_rgb(t_game *game, int i, char *s)
@@ -45,7 +44,6 @@ void	check_rgb(t_game *game, int i, char *s)
 	int		n;
 	int		c;
 
-	j = 0;
 	n = 0;
 	c = 0;
 	j = 0;
@@ -53,16 +51,17 @@ void	check_rgb(t_game *game, int i, char *s)
 		j++;
 	while (!ft_isdigit(game->board[i][j]) && game->board[i][j] != '-')
 		++j;
-	while (j < ft_strlen(game->board[i]) && ++c <= 3)
+	while (++c < 4)
 	{
 		n = ft_atoi(&game->board[i][j]);
 		init_rgb(game, n, s, c);
-		if (n > 255 || n < 0)
-			error_exit("RGB is wrong");
-		while (game->board[i][j] != ',')
-			++j;
+		if (c < 3)
+		{
+			while (game->board[i][j] != ',')
+				++j;
+		}
 		++j;
 	}
-	if (c != 3)
+	if (c != 4)
 		error_exit("No RGB value submited!");
 }
